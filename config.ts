1a20309e3bfd3185b9e0f5dfc37c53c92ad82cf3
IanReyes2 <ian.vincent.reyes@adamson.edu.ph>
2025-10-06 15:27:11 +0800

    Update config.ts

--- config.ts
+++ config.ts
@@ -2,5 +2,5 @@
 const hostname = typeof window !== "undefined" ? window.location.hostname : "localhost";
 
 export const API_URL = `http://${hostname}:3001`;
-export const WS_URL = `http://192.168.1.4:3001`; // localhost if testing locally
+export const WS_URL = `http://192.168.140.153:3001`; // localhost if testing locally
 
