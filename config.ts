62a7d4e657b2ef8392c96ab3398152674d1a863e
IanReyes2 <ian.vincent.reyes@adamson.edu.ph>
2025-10-14 21:24:27 +0800

    it works too :)

--- config.ts
+++ config.ts
@@ -2,5 +2,8 @@
 const hostname = typeof window !== "undefined" ? window.location.hostname : "localhost";
 
 export const API_URL = `http://${hostname}:3001`;
-export const WS_URL = `ws://192.168.254.124:3001`; // localhost if testing locally
+export const WS_URL = `ws://192.168.254.124:3001`; // this is the globe wifi
 
+// export const WS_URL = `ws://192.168.1.9:3001`; this is the home wifi
+
+// remove or add double slashes at the end of the URLs as needed
